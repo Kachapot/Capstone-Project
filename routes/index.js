ef93a03f33e774d721ecf2145e52642c557048b6
Kachapot <75522599+Kachapot@users.noreply.github.com>
2023-02-16 23:09:50 +0700

    up

--- routes/index.js
+++ routes/index.js
@@ -28,27 +28,74 @@ router.get('/main',authorization,async(req,res)=>{
   try {
     const body = req.query
     console.log('body',body);
+    let startDate = body.startDate
+    let endDate = body.endDate
+    if(endDate < startDate){
+      startDate = body.endDate
+      endDate = body.startDate
+    }
     const today = moment().format('YYYY-MM-DD')
     const startMonth = moment().startOf('month').format('YYYY-MM-DD')
     const endMonth = moment().endOf('month').format('YYYY-MM-DD')
-    const startYear = moment().startOf('year').format('YYYY-MM-DD')
-    const endYear = moment().endOf('year').format('YYYY-MM-DD')
-    // const totalSell = await db('tb_order_sell_detail').select(db.raw(`
-    // format(sum(total),2) as total,
-    // date_format(date,'%m-%y') as month
-    // `)).whereRaw(`
-    // date >= '${startMonth}' and date <= '${endMonth}
-    // `)
-    // .groupBy('month')
-    // .orderBy('month')
-    // console.log('totalSell',totalSell);
+    // const startYear = moment().startOf('year').format('YYYY-MM-DD')
+    // const endYear = moment().endOf('year').format('YYYY-MM-DD')
+    const totalSell = await db('tb_order_sell_detail').select(
+      db.raw(`
+      format(sum(total),2) as total,
+      date_format(date,'%m') as month`)
+      )
+    .whereRaw(`date between '${startMonth}' and '${endMonth}'`)
+    .groupBy('month')
+    .orderBy('month')
+    console.log('totalsell',totalSell);
+    for (let index = 0; index < totalSell.length; index++) {
+      const element = totalSell[index];
+      if(element.month == '01'){
+        
+      }
+      if(element.month == '02'){
+
+      }
+      if(element.month == '03'){
+
+      }
+      if(element.month == '04'){
+
+      }
+      if(element.month == '05'){
+
+      }
+      if(element.month == '06'){
+
+      }
+      if(element.month == '07'){
+
+      }
+      if(element.month == '08'){
+
+      }
+      if(element.month == '09'){
+
+      }
+      if(element.month == '10'){
+
+      }
+      if(element.month == '11'){
+
+      }
+      if(element.month == '12'){
+
+      }
+      
+    }
+
       let data = {
         status:true,
         menu:req.menu,
         msg:"ยินดีต้อนรับ",
         username:req.admin,
-        startDate:today,
-        endDate:today
+        startDate:body.startDate??today,
+        endDate:body.endDate??today
       }
       res.render('index',data)
     
